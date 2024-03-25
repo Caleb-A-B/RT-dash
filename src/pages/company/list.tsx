8c2758902a89b0184b1ca9f36fc1f3bfb575c502
Caleb B <calebabaca@gmail.com>
2024-03-25 11:39:04 -0700

    company list

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -1,7 +1,12 @@
+import CustomAvatar from '@/components/custom-avatar';
+import { Text } from '@/components/text';
 import { COMPANIES_LIST_QUERY } from '@/graphql/queries';
-import { CreateButton, List, useTable } from '@refinedev/antd'
-import { useGo } from '@refinedev/core';
-import { Table } from 'antd';
+import { Company } from '@/graphql/schema.types';
+import { currencyNumber } from '@/utilitis';
+import { SearchOutlined } from '@ant-design/icons';
+import { CreateButton, DeleteButton, EditButton, FilterDropdown, List, useTable } from '@refinedev/antd'
+import { getDefaultFilter, useGo } from '@refinedev/core';
+import { Space, Table } from 'antd';
 
 export const CompanyList = () => {
   const go = useGo();
@@ -41,7 +46,45 @@ export const CompanyList = () => {
         ...tableProps.pagination,
       }}
       >
-
+      <Table.Column<Company>
+      dataIndex="name"
+      title="Company Title"
+      defaultFilteredValue={getDefaultFilter('id', filters)}
+      filterIcon={<SearchOutlined />}
+      filterDropdown={ (props) => (
+        <FilterDropdown {...props} >
+          <input placeholder="Search Company" />
+        </FilterDropdown>
+      ) }
+      render={(value, record) => (
+        <Space>
+          <CustomAvatar shape="square" name={record.name} src={record.avatarUrl} />
+          <Text style={{ whiteSpace: 'nowrap'}} >
+            {record.name}
+          </Text>
+        </Space>
+      )}
+      />
+      <Table.Column<Company>  
+      dataIndex="totalRevenue"
+      title="Estimated deals amount"
+      render={(value, company) => (
+        <Text>
+          {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
+        </Text>
+      )}
+      />
+       <Table.Column<Company>  
+      dataIndex="id"
+      title="Actions"
+      fixed="right"
+      render={(value) => (
+      <Space>
+        <EditButton hideText size="small" recordItemId={value} />
+        <DeleteButton hideText size="small" recordItemId={value} />
+      </Space>
+      )}
+      />
       </Table>
 
     </List>
